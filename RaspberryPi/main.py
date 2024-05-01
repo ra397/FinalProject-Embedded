c484cf2a2213c5d6b405817621fc468777831546
ra397 <rabialaya@gmail.com>
2024-05-01 00:08:05 -0500

    Added 2 display modes on LCD

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,5 +1,5 @@
 from lcdlib import *
-import time
+import utime
 from machine import ADC, Pin, Timer
 import urequests
 from connect_to_wifi import *
@@ -13,6 +13,15 @@ lcd.clrscr()
 
 # Initialize the sensor
 temp_sensor = ADC(Pin(28))  # ADC on GP28/ADC2
+
+# Initialize the joystick
+xAxis = ADC(Pin(27))
+yAxis = ADC(Pin(26))
+button = Pin(17,Pin.IN, Pin.PULL_UP)
+
+displayMode = 0
+oldDisplayMode = 0
+timeCount = 0
 
 # Connect pico w to internet
 connect_to_internet('ala247', 'coolfinch947')
@@ -39,20 +48,67 @@ while True:
     # Get tempreture reading
     temp = read_temperature(temp_sensor)
     
+    # Get Joystick reading
+    xValue = xAxis.read_u16()
+    yValue = yAxis.read_u16()
+    buttonValue = button.value()
+
+    # Update xStatus based on xValue
+    if xValue <= 600:
+        xStatus = "left"
+    elif xValue >= 60000:
+        xStatus = "right"
+    else:
+        xStatus = "middle"  
+
+    # Update yStatus based on yValue
+    if yValue <= 600:
+        yStatus = "up"
+    elif yValue >= 60000:
+        yStatus = "down"
+    else:
+        yStatus = "middle"  
+
+    # Update buttonStatus based on buttonValue
+    if buttonValue == 0:
+        buttonStatus = "pressed"
+        if displayMode == 0:
+            displayMode = 1
+        else:
+            displayMode = 0
+    else:
+        buttonStatus = "not pressed"
+    
     # Dispaly to LCD
-    lcd.pos_puts(0, 0, "Temp: {:.2f} C".format(temp[0]))
-    lcd.pos_puts(6, 1, "{:.2f} F".format(temp[1]))
+    print(displayMode)
+    if displayMode == 0:
+        if oldDisplayMode != displayMode: # clear screen if dispaly mode has changed
+            lcd.clrscr()
+        lcd.pos_puts(0, 0, "Temp: {:.2f} C".format(temp[0]))
+        lcd.pos_puts(6, 1, "{:.2f} F".format(temp[1]))
+    else:
+        if oldDisplayMode != displayMode: # clear screen if dispaly mode has changed
+            lcd.clrscr()
+        lcd.pos_puts(0, 0, "Weather Forecast:")
+        lcd.pos_puts(0, 1, "Scroll w Joystick")
     
     # Send data to server
-    data = {
-        'c_temperature': temp[0],
-        'f_temperature': temp[1]
-        }
-    try:
-        response = urequests.post(url, json=data)  # Send data as JSON
-        print(response.text)
-        response.close()
-    except Exception as e:
-        print("Failed to send data:", e)
+    if timeCount == 100:
+        lcd.clrscr()
+        lcd.pos_puts(0, 0, "Sending data")
+        lcd.pos_puts(0, 1, "to server ...")
+        data = {
+            'c_temperature': temp[0],
+            'f_temperature': temp[1]
+            }
+        try:
+            response = urequests.post(url, json=data)  # Send data as JSON
+            print(response.text)
+            response.close()
+        except Exception as e:
+            print("Failed to send data:", e)
+        timeCount = 0
     
-    time.sleep(1)
+    oldDisplayMode = displayMode
+    timeCount += 1
+    utime.sleep(0.1)
